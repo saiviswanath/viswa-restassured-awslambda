650aafd75496dedacb45fb86af756295cee63277
Viswa <viswa@c.com>
2024-12-03 11:40:59 +0530

    init Signed-off-by: Viswa <viswa@c.com>

--- src/main/java/com/booking/awslambda/AWSLambdaApiTestHandler.java
+++ src/main/java/com/booking/awslambda/AWSLambdaApiTestHandler.java
@@ -1,7 +1,6 @@
 package com.booking.awslambda;
 
 import java.util.List;
-import java.util.Map;
 
 import org.testng.TestNG;
 import org.testng.collections.Lists;
@@ -10,31 +9,43 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import software.amazon.awssdk.services.lambda.LambdaClient;
-import software.amazon.awssdk.services.lambda.model.LambdaException;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-public class AWSLambdaApiTestHandler implements RequestHandler<Map<String,String>, String> {
-    private static final LambdaClient lambdaClient = LambdaClient.builder().build();
+public class AWSLambdaApiTestHandler implements RequestHandler<String, String> {
+    //private static final LambdaClient lambdaClient = LambdaClient.builder().build();
     @Override
-    public String handleRequest(Map<String,String> event, Context context) {
+    public String handleRequest(String event, Context context) {
 
         LambdaLogger logger = context.getLogger();
         logger.log("Handler invoked");
         
+        String[] arr = event.split(",");
+        
         TestNG testng = new TestNG();
         List<String> suites = Lists.newArrayList();
-        suites.add("booking-testng.xml");//path to xml..
+        
+        for (String s : arr) {
+        	logger.log("Adding " + s + " to suite execution");
+	        ClassLoader classLoader = getClass().getClassLoader();
+	        String testngpath = classLoader.getResource(s).getPath();
+	        suites.add(testngpath);//path to xml..
+        }
+        
         testng.setTestSuites(suites);
         testng.run();
         
         //TODO: Reports to AWS S3 post suite run using s3 sdk
-
-        String response = null;
-        try {
-            response = lambdaClient.serviceName();
-        } catch(LambdaException e) {
-            logger.log(e.getMessage());
-        }
-        return response;
+        S3ClientBuilder s3 = S3Client.builder()
+                .region(Region.AP_SOUTH_1);
+        PutObjectRequest putOb = PutObjectRequest.builder()
+                .bucket("restassuredlambda")
+                .key("")
+                .build();
+        // https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/javav2/example_code/s3/src/main/java/com/example/s3/PutObjectMetadata.java
+        
+        return "Success";
     }
 }
